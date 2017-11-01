44ed58a6cdd2ad2d488a06426c98784cc5bf8e06
laricampos <lcampos@samsao.co>
2017-10-31 21:17:08 -0400

    Add call phone feature

--- app/src/main/java/com/samsao/sandbox/MainActivity.java
+++ app/src/main/java/com/samsao/sandbox/MainActivity.java
@@ -1,9 +1,12 @@
 package com.samsao.sandbox;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +16,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.main_telephone_button)
+    void onTelephoneButtonClick() {
+        Intent intent = new Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", "5144321234", null));
+        startActivity(intent);
     }
 }
